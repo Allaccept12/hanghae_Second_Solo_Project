2bdc3199c8b1a0f2a3109eb58ca54142b9499ed8
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-02-21 19:42:13 +0900

    [FEAT] 로그인 및 회원가입 로그인 상태에서 요청시 fail 및 msg Response

--- src/main/java/second/solo/controller/AccountController.java
+++ src/main/java/second/solo/controller/AccountController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import second.solo.advice.ApiRequestException;
 import second.solo.advice.Success;
 import second.solo.dto.request.AccountLoginRequestDto;
 import second.solo.dto.request.AccountRegisterRequestDto;
+import second.solo.jwt.UserDetailsImpl;
 import second.solo.service.AccountService;
 
 @RestController
@@ -22,13 +24,21 @@ public class AccountController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<Success> login(@RequestBody AccountLoginRequestDto requestDto) {
+    public ResponseEntity<Success> login(@RequestBody AccountLoginRequestDto requestDto,
+                                         @AuthenticationPrincipal UserDetailsImpl accountDetails) {
+        if (accountDetails != null) {
+            throw new ApiRequestException("이미 로그인 되어있습니다.");
+        }
         return new ResponseEntity<>(new Success<>(
                 "로그인 성공", accountService.login(requestDto)), HttpStatus.OK);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<Success> register(@RequestBody AccountRegisterRequestDto requestDto) {
+    public ResponseEntity<Success> register(@RequestBody AccountRegisterRequestDto requestDto,
+                                            @AuthenticationPrincipal UserDetailsImpl accountDetails) {
+        if (accountDetails != null) {
+            throw new ApiRequestException("이미 로그인 되어있습니다.");
+        }
         return new ResponseEntity<>(new Success<>(
                 "회원가입 성공", accountService.registerAccount(requestDto)), HttpStatus.OK);
     }
