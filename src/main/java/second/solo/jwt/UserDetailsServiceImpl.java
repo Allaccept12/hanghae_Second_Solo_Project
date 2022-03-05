f9617b042e9b44d2ea45d6ef36ac213d3f3cc82b
Jay_ <79740505+Allaccept12@users.noreply.github.com>
2022-03-06 00:01:15 +0900

    [FEAT] beanstalk 설정

--- src/main/java/second/solo/jwt/UserDetailsServiceImpl.java
+++ src/main/java/second/solo/jwt/UserDetailsServiceImpl.java
@@ -17,7 +17,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final AccountRepository accountRepository;
 
     public UserDetails loadUserByUsername(String userPk) throws UsernameNotFoundException {
-        log.info(userPk);
         Account user = accountRepository.findById(Long.parseLong(userPk))
                 .orElseThrow(() -> new ApiRequestException("존재하지 않거나 로그인 하지 않았습니다"));
         return new UserDetailsImpl(user);
